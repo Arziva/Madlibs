fbd777f78f261096163d23b4caaa0e0fa415b76b
Arziva <84022433+Arziva@users.noreply.github.com>
2022-07-19 22:13:33 +0530

    Update madlibs.py

--- madlibs.py
+++ madlibs.py
@@ -14,10 +14,3 @@ string = f"The world is so {adj}! \
     It makes me super {verb1} and {verb2}"
 
 print(string)
-
-from sample_madlibs import zombie, hungergames, hp, code
-import random
-
-if __name__  == "main":
-    m = random.choice([hp,code,zombie,hungergames])
-    m.madlib()
