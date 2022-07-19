c4db29888c0e9024c6d84f3a1faa6beb83882745
Arziva <84022433+Arziva@users.noreply.github.com>
2022-07-19 20:55:05 +0530

    Create madlibs.py

---
+++ madlibs.py
@@ -0,0 +1,17 @@
+#string concatenation
+
+student = ""
+
+print("The best coder is: " + student)
+print("The best coder is:{}".format(student))
+print(f"The best coder is:{student}")
+
+adj = input("Adjective:")
+verb1 = input("verb1:")
+verb2 = input("verb2:")
+
+string1 = f"The world is so {adj}! \
+    It makes me super {verb1} and {verb2}"
+
+print(string1)
+
